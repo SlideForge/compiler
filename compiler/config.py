cf21af125788b59802ca3d79570c844fefb60a05
LoC <loc@locrealloc.de>
2023-04-15 20:03:53 +0200

    feat: added configuration options and log level reader

--- compiler/config.py
+++ compiler/config.py
@@ -1,11 +1,25 @@
-from os import getenv
+from typing import Any
 from pathlib import Path
 from yaml import safe_load
+import logging
 
 
 class Config:
 	REPO_LINK: str
-	LOG_LEVEL: str
+	LOG_LEVEL: int
+	LOG_FILE: str
+
+
+def load_log_level(log_level: str | int) -> int:
+	if isinstance(log_level, int):
+		return log_level
+
+	num_log_level = getattr(logging, log_level.upper())
+
+	if not isinstance(num_log_level, int):
+		raise ValueError(f"Invalid log level: {log_level}")
+
+	return num_log_level
 
 
 def load_config(path: Path) -> None:
@@ -18,4 +32,5 @@ def load_config(path: Path) -> None:
 		config = safe_load(config_file)
 
 	Config.REPO_LINK = config.repo
-	Config.LOG_LEVEL = config.log_level
+	Config.LOG_LEVEL = load_log_level(config.log.level)
+	Config.LOG_FILE = config.log.file
